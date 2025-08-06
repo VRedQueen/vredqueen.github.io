a23b4f835ce9feaea6ec53ebc92e97f6da3d15d5
RedQueen <88919234+VRedQueen@users.noreply.github.com>
2025-08-05 23:03:24 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://redqueen.github.io",
-	base:"/",
+	base:"/redqueen.github.io",
 	outDir: "./dist",
 	
 	trailingSlash: "always",
